13d841403fdd35ca11863fd544efee14845a7aaf
Emanuele <cannone.emanuele@gmail.com>
2021-03-01 16:45:59 +0100

    database completo

--- database.php
+++ database.php
@@ -9,5 +9,34 @@
         [
             'name' => 'split',
             'image' => 'img/split.jpeg'
+        ],
+        [
+            'name' => 'cure',
+            'image' => 'img/cure.jpeg'
+        ],
+        [
+            'name' => 'aquietplace',
+            'image' => 'img/aquietplace.jpeg'
+        ],
+        [
+            'name' => 'metal_lifting',
+            'image' => 'img/metal_lifting.jpg'
+        ],
+        [
+            'name' => 'player_left',
+            'image' => 'img/player_left.jpg'
+        ],
+        [
+            'name' => 'sinister',
+            'image' => 'img/sinister.jpeg'
+        ],
+        [
+            'name' => 'stranger',
+            'image' => 'img/stranger.jpeg'
+        ],
+        [
+            'name' => 'youg',
+            'image' => 'img/youg.jpeg'
         ]
+
     ];
