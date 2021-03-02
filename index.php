ebeb93b6a48eb01654cea6071243ca2d99aa2c00
Emanuele <cannone.emanuele@gmail.com>
2021-03-02 15:45:57 +0100

    layout

--- index.php
+++ index.php
@@ -4,35 +4,41 @@
 
 ?>
 
-
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <link rel="stylesheet" href="dist/style.css?v=<?php echo time(); ?>">
     <title>php-ajax-dischi</title>
 </head>
 <body>
     
+    <header>
+        <div class="logo">
+            <img src="img/logo.jpg" alt="logo">
+        </div>
+    </header>
 
-    <?php foreach ($dischi as $element) { ?>
-        
+    <main>
 
-        <div class="etichetta">
+        <div class="box">
+        <?php foreach ($dischi as $element) { ?>
 
-            <h2><?php echo $element['name']?></h2>
-            <img src="<?php echo $element['image']; ?>" alt="">
-
+            <div class="etichetta">
+                
+                <h2><?php echo $element['name']?></h2>
+                <img src="<?php echo $element['image']; ?>" alt="">
+                
+            </div>
+            
+        <?php }?>
         </div>
 
+    </main>
 
-
-
-    <?php }?>
+    <footer></footer>
     
 
 </body>
