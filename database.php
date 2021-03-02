8eff7f0ba2896ba93ff90d82baf59e83a643dd9c
Emanuele <cannone.emanuele@gmail.com>
2021-03-02 16:53:55 +0100

    filtro generi

--- database.php
+++ database.php
@@ -4,39 +4,48 @@
 
         [
             'name' => 'einaudi',
-            'image' => 'img/einaudi.jpeg'
+            'image' => 'img/einaudi.jpeg',
+            'genere' => 1
         ],
         [
             'name' => 'split',
-            'image' => 'img/split.jpeg'
+            'image' => 'img/split.jpeg',
+            'genere' => 1
         ],
         [
             'name' => 'cure',
-            'image' => 'img/cure.jpeg'
+            'image' => 'img/cure.jpeg',
+            'genere' => 3
         ],
         [
             'name' => 'aquietplace',
-            'image' => 'img/aquietplace.jpeg'
+            'image' => 'img/aquietplace.jpeg',
+            'genere' => 2
         ],
         [
             'name' => 'metal_lifting',
-            'image' => 'img/metal_lifting.jpg'
+            'image' => 'img/metal_lifting.jpg',
+            'genere' => 3
         ],
         [
             'name' => 'player_left',
-            'image' => 'img/player_left.jpg'
+            'image' => 'img/player_left.jpg',
+            'genere' => 2
         ],
         [
             'name' => 'sinister',
-            'image' => 'img/sinister.jpeg'
+            'image' => 'img/sinister.jpeg',
+            'genere' => 2
         ],
         [
             'name' => 'stranger',
-            'image' => 'img/stranger.jpeg'
+            'image' => 'img/stranger.jpeg',
+            'genere' => 2
         ],
         [
             'name' => 'youg',
-            'image' => 'img/youg.jpeg'
+            'image' => 'img/youg.jpeg',
+            'genere' => 1
         ]
 
     ];
--- src/app.js
+++ src/app.js
@@ -1,14 +1,36 @@
 var app = new Vue({
     el: '#app',
     data:{
-        arrayRisultato: []
+        arrayRisultato: [],
+        arrayGenere: ['All'],
+        numeroGenere: 0,
     },
     mounted(){
         axios
             .get('server.php')
             .then( result => {
+
                 this.arrayRisultato = result.data;
-                console.log(this.arrayRisultato);
-            })
+                this.numeroGenere = 'All'; 
+
+                this.arrayRisultato.forEach(element => {
+
+                    if (!this.arrayGenere.includes(element.genere)) {
+
+                        this.arrayGenere.push(element.genere);
+
+                    }
+                    
+                });
+            
+            });
+    },
+    methods: {
+
+        ciao(){
+            console.log(this.numeroGenere);
+        }
+        
     }
+
 });
